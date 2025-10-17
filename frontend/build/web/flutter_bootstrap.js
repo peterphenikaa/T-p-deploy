6786aa6cc378ca548e78777e98f013951b9773f7
peterphenikaa <23010678@st.phenikaa-uni.edu.vn>
2025-10-18 01:41:41 +0700

    m

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "400504221"
+    serviceWorkerVersion: "2087809049"
   }
 });
--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d9438595f9f546b144afe6f8b1a8c60c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "c6e2d1d9ad4fe89e30efecf8445882b2",
+"flutter_bootstrap.js": "54330d0e9cbc92a01393ee3bd7f2ac3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "42af579c7265646aca06a68e4334f26a",
 "/": "42af579c7265646aca06a68e4334f26a",
-"main.dart.js": "bb6c3b59c0a51274a06c86eca66aed60",
+"main.dart.js": "a1ea9263ceaeeb91f14279e756fe4bd0",
 "manifest.json": "848e20f722707125ac8b64e5081adc85",
 "version.json": "f9a9ae135c39399785f34b2ea788e23b"};
 // The application shell files that are downloaded before a service worker can
